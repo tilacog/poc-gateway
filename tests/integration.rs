6bedeedccc45174b4b494b41c00eacf468bdb32c
tilacog <tilacog@gmail.com>
2023-11-25 17:53:34 -0300

    integration tests

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,4 +1,6 @@
 use solana_program_test::{processor, tokio, ProgramTest};
+use solana_sdk::signature::{Keypair, Signer};
+use solana_sdk::transaction::Transaction;
 
 fn program_test() -> ProgramTest {
     ProgramTest::new(
@@ -11,4 +13,8 @@ fn program_test() -> ProgramTest {
 #[tokio::test]
 async fn test_queue_message() {
     let (mut banks_client, payer, recent_blockhash) = program_test().start().await;
+
+    let mut transaction = Transaction::new_with_payer(&[], Some(&payer.pubkey()));
+    transaction.sign(&[&payer], recent_blockhash);
+    banks_client.process_transaction(transaction).await.unwrap();
 }
