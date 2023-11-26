d1ab97e276e9940e306c4941d8148c1fe0ca0087
tilacog <tilacog@gmail.com>
2023-11-25 21:21:15 -0300

    working tests

--- src/instruction.rs
+++ src/instruction.rs
@@ -101,31 +101,12 @@ pub fn queue(
 }
 
 #[cfg(test)]
-mod tests {
-    use rand::distributions::{Alphanumeric, Standard};
-    use rand::Rng;
+pub mod tests {
+
+    use test_utilities::{rand_array, rand_str};
 
     use super::*;
     use crate::error::GatewayError;
-
-    // Helper function to generate a random byte array
-    fn rand_array<const SIZE: usize>() -> [u8; SIZE] {
-        let rng = rand::thread_rng();
-        rng.sample_iter(Standard)
-            .take(SIZE)
-            .collect::<Vec<u8>>()
-            .try_into()
-            .unwrap()
-    }
-
-    // Function to generate a random alphanumeric string
-    fn rand_str(len: usize) -> String {
-        let rng = rand::thread_rng();
-        rng.sample_iter(&Alphanumeric)
-            .take(len)
-            .map(char::from)
-            .collect()
-    }
 
     #[test]
     fn unpack() {
